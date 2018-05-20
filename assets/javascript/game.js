2430984aae79a57ea0ae59aa36fcf14f75308a82
MondoBiscuit <bradleofabian@gmail.com>
2018-05-20 18:06:38 -0400

    computerGuess not running

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,4 @@
 // letters used for computeer to guess
 var computerChoice = ["a", "b", "c" ,"d" ,"e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"] ;
+//allows computer to guess letter 
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
