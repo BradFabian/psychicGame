87d042d46c4bd206951c0a444cf04cc332082ecb
MondoBiscuit <bradleofabian@gmail.com>
2018-05-24 14:14:58 -0400

    JavaScript added documet.onkeyup and if else statement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,15 +2,36 @@
 var computerChoice = ["a", "b", "c" ,"d" ,"e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"] ;
 //allows computer to guess letter 
 var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+
 //Setting all to zero
 var wins = 0;
 var losses = 0;
-var guesses = 9;
-var guessesLeft = 9;
-var guessedLetters = [];
-var letterToGuess = [];
 
-//Allows the user 9 guesses
-// guesses = guesses || 9
+var guessesLeft = 9;
+var guessedSoFar = [];
+
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); //taking in user guess
+    guessesSoFar.push(userGuess); //pushing user guess to guesses so far
+    if (userGuess == computerGuess) {
+        wins++;
+        alert('Way to go! You\'ve guesesed corrrectly. You Won!');
+        guessesLeft = 9; //reseting the guesses back to 9 so that the user can play again
+        guessesSoFar.length = 0; //this removes everything from the guesses so far array, so that the guesses from the previous round don't show
+    }
+    else if (guessesLeft == 0){
+        losses++;
+        alert('You didn\'t guess the correct letter. You lost. Refresh to try again.');
+        guessesLeft = 9;
+        guessesSoFar.length = 0;
+    }
+    else if (userGuess !== computerGuess){
+        guessesLeft--; //decrementing the guesses left
+    }  
+   
+}
+
+
+
 
                                                
